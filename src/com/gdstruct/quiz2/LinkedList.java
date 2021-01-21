316cd0903b357a00c9caae88c90f72c315a14ffb
KoolieAid <erikrupert.go@benilde.edu.ph>
2021-01-21 21:04:51 +0800

    Module 2 commit

---
+++ src/com/gdstruct/quiz2/LinkedList.java
@@ -0,0 +1,100 @@
+package com.gdstruct.quiz2;
+
+public class LinkedList {
+
+    private Node head;
+    private int size = 1; //sabi raw size variable pero gumawa ako ng size() funciton lol
+
+    public LinkedList(int data)
+    {
+        head = new Node(data);
+    }
+
+    public void removeFirstElement()
+    {
+        head = head.getNextNode();
+        head.setPrevNode(null);
+        size--;
+    }
+
+    public void addNode(int data)
+    {
+        Node current = head;
+        Node lastIteration = null;
+        Node newNode = new Node(data);
+        while (current != null)
+        {
+            lastIteration = current;
+            current = current.getNextNode();
+        }
+
+        lastIteration.setNextNode(newNode);
+        lastIteration.setPrevNode(current);
+        newNode.setNextNode(null);
+
+        size++;
+    }
+
+    public void removeAtIndex(int index)
+    {
+        Node headIteration = head;
+        Node previousNode = headIteration;
+        for (int i = 0; i < index; i++)
+        {
+            previousNode = headIteration;
+            headIteration = headIteration.getNextNode();
+        }
+        previousNode.setNextNode(headIteration.getNextNode());
+        headIteration.getNextNode().setPrevNode(previousNode);
+    }
+
+    public int size()
+    {
+        int count = 0;
+        Node headIteration = head;
+        while (headIteration != null)
+        {
+            count++;
+            headIteration = headIteration.getNextNode();
+        }
+        return count;
+    }
+
+    public boolean contains(Node search)
+    {
+        Node headIteration = head;
+        while (headIteration != null)
+        {
+            if (headIteration.getValue() == search.getValue())
+            {
+                return true;
+            }
+            headIteration = headIteration.getNextNode();
+        }
+        return false;
+    }
+
+    public int indexOf(Node search)
+    {
+        Node headIteration = head;
+        for (int i = 0; i < this.size(); i++)
+        {
+            if (headIteration.getValue() == search.getValue())
+            {
+                return i;
+            }
+            headIteration = headIteration.getNextNode();
+        }
+        return -1;
+    }
+
+    public void printAll()
+    {
+        Node headIte = head;
+        for (int i = 0; i < this.size(); i++)
+        {
+            System.out.println(headIte.getValue());
+            headIte = headIte.getNextNode();
+        }
+    }
+}
---
+++ src/com/gdstruct/quiz2/Main.java
@@ -0,0 +1,23 @@
+package com.gdstruct.quiz2;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        LinkedList mahList = new LinkedList(10);
+        mahList.addNode(20);
+        mahList.addNode(30);
+        mahList.addNode(40);
+        mahList.addNode(50);
+        System.out.println(mahList.size());
+
+        System.out.println(mahList.contains(new Node(20)));
+        System.out.println(mahList.indexOf(new Node(20)));
+
+        mahList.printAll();
+        mahList.removeAtIndex(1);
+        System.out.println();
+        mahList.printAll();
+
+    }
+}
